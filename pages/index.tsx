73bf31c2a702ed2c80ff17bd31d234214b9e0e59
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-03 21:42:53 +0000

    Create the Results comonent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 //import "../styles/globals.css";
 
 
@@ -16,11 +17,18 @@ export default function Home() {
         <Header/>
       {/* Navbar */}
       <Nav/>
-
       {/* Results */}
+      <Results />
+
 
 
     </div>
     
   );
+}
+
+export async function getServerSideProps(context: any) {
+
+  const genre= context.query.genre
+  
 }
