3ddbdc8ed754a7b5dcd1d9c909c2e3647be21898
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-03 22:12:43 +0000

    Errors Fixed - HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH *-_-*

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
+import  requests from "../utils/requests";
 //import "../styles/globals.css";
 
 
-export default function Home() {
+export default function Home(props: any) {
+  console.log(props)
   return (
     <div>
       <Head>
@@ -29,6 +31,17 @@ export default function Home() {
 
 export async function getServerSideProps(context: any) {
 
-  const genre= context.query.genre
+  const genre= context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url 
+    }`
+    ).then((res) => res.json());
+  return{
+    props:{
+      results: request.results,
+    }
+  }
   
 }
